bc29399f0beb968de466348a7e7a888ba55dcae3
medeirosavio <savio.medeiros@ccc.ufcg.edu.br>
2024-09-19 19:55:57 -0300

    Finalizando teste

--- src/main/java/org/example/controller/TransferenciaController.java
+++ src/main/java/org/example/controller/TransferenciaController.java
@@ -1,8 +1,11 @@
 package org.example.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.dto.NotificationRequestDTO;
+import org.example.service.NotificationService;
 import org.example.service.TransferenciaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +18,8 @@ public class TransferenciaController {
 
     @Autowired
     private TransferenciaService transferenciaService;
+    @Autowired
+    private NotificationService notificationService;
 
     @PostMapping("/realizar")
     public ResponseEntity<String> realizarTransferencia(
@@ -25,16 +30,31 @@ public class TransferenciaController {
         boolean autorizado = transferenciaService.verificarAutorizacao();
         if (autorizado) {
             transferenciaService.realizarTransferencia(remetenteCpf, destinatarioCpf, valor);
+            NotificationRequestDTO remetenteNotification = new NotificationRequestDTO(
+                    remetenteCpf, "Transferência realizada com sucesso!");
+            notificationService.sendNotification(remetenteNotification);
             return ResponseEntity.ok("Transferência realizada com sucesso!");
         } else {
             return ResponseEntity.status(400).body("Transferência não autorizada.");
         }
     }
 
-    @GetMapping("/autorizacao")
+    @GetMapping("/autorizar")
     public ResponseEntity<String> verificarAutorizacao() {
         boolean autorizado = transferenciaService.verificarAutorizacao();
         return ResponseEntity.ok("Autorizado");
+    }
+
+
+    @PutMapping("/notificar")
+    public ResponseEntity<String> enviarNotificacao(@RequestBody NotificationRequestDTO notificationRequest) {
+        boolean sucesso = notificationService.sendNotification(notificationRequest);
+
+        if (sucesso) {
+            return ResponseEntity.ok("Notificação enviada com sucesso.");
+        } else {
+            return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body("Falha ao enviar notificação.");
+        }
     }
 
 }
